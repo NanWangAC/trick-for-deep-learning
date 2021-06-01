f1bd78eda5318e6ea901c1df83a4962f31ffd50f
nanwangac <nanwang.ac@gmail.com>
2021-06-01 10:51:51 +0800

    delete old

--- clip_forPpredict.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# predict
-y_pred = model.predict(data)
-
-# bound for predict clipping
-lower_bound, upper_bound = 0.025, 0.975
-y_pred_clip = np.clip(y_pred, lower_bound, upper_bound)
