7eaaf34e55434ece2bba138b835fd3fedf751b03
nanwangac <nanwang.ac@gmail.com>
2021-06-01 11:28:38 +0800

    add comment

--- clip_for_predict.py
+++ clip_for_predict.py
@@ -3,6 +3,6 @@ import numpy as np
 # predict
 y_pred = model.predict(data)
 
-# bound for predict clipping
+# bound for predict clipping make performance better
 lower_bound, upper_bound = 0.025, 0.975
 y_pred_clip = np.clip(y_pred, lower_bound, upper_bound)
