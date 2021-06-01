3facd6776e370ff1aefaeb48611ca04e7b57d695
nanwangac <nanwang.ac@gmail.com>
2021-06-01 14:36:40 +0800

    description

--- clip_for_predict.py
+++ clip_for_predict.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+"""
+clipping explain:
+If you predict something to be true when it turns out to be false, your error score increases infinitely.  
+If we can keep the probability between 0.05 and 0.95(or other feasible value), 
+then we can never be quite sure about the prediction.  
+In this case, we do not see a large increase in error functions.  
+"""
 # predict
 y_pred = model.predict(data)
 
